525bfcaa99e45f9feadd86a08540721f47be511a
davig-sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-02-17 19:08:38 -0300

    Updates

---
+++ precache-manifest.171a819027bd8cb6adcaefe78268a8a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa31314d139e5e7daa504e203dec658",
+    "url": "/doeteca/index.html"
+  },
+  {
+    "revision": "019d7f3c72ffdffae9d3",
+    "url": "/doeteca/static/css/2.b9d25339.chunk.css"
+  },
+  {
+    "revision": "f6b28f90593665f394a9",
+    "url": "/doeteca/static/css/main.647af688.chunk.css"
+  },
+  {
+    "revision": "019d7f3c72ffdffae9d3",
+    "url": "/doeteca/static/js/2.6c1be262.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/doeteca/static/js/2.6c1be262.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b28f90593665f394a9",
+    "url": "/doeteca/static/js/main.f273f76c.chunk.js"
+  },
+  {
+    "revision": "10b233e745c995f7a983",
+    "url": "/doeteca/static/js/runtime-main.0f326f21.js"
+  },
+  {
+    "revision": "9ac31ecfa8d835d95e813ef009c99f06",
+    "url": "/doeteca/static/media/book.9ac31ecf.png"
+  },
+  {
+    "revision": "ebbc0ed07976f1fdb5c3cfc4a4f5a67b",
+    "url": "/doeteca/static/media/books1.ebbc0ed0.jpg"
+  },
+  {
+    "revision": "034bec09e276c9e338325f5fc76fc663",
+    "url": "/doeteca/static/media/books2.034bec09.jpg"
+  },
+  {
+    "revision": "e9c291483b92688d6c3cac19d457b257",
+    "url": "/doeteca/static/media/cover.e9c29148.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/doeteca/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/doeteca/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/doeteca/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/doeteca/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/doeteca/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "48bd15d79a85cffd6c02636d3f4bc594",
+    "url": "/doeteca/static/media/logo.48bd15d7.png"
+  }
+]);
